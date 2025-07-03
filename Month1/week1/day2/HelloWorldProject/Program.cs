69602a170927d821b39d0244afe3cfa3b132851d
debra <deltaburs@gmail.com>
2025-07-03 16:36:02 +0330

    get input from user

---
+++ Month1/week1/day2/HelloWorldProject/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+using System;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("Enter the first Number: ");
+        String input1 = Console.ReadLine();
+
+        int num1 = int.Parse(input1);
+
+        Console.WriteLine("Enter the second number: ");
+
+        String input2 = Console.ReadLine();
+
+        int num2 = int.Parse(input2);
+
+        int result = num1 + num2;
+
+        Console.WriteLine($"The result is {result}");
+    }
+}
